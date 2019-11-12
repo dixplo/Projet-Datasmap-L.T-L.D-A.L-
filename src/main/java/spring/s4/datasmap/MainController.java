fee50af8e1b50a8a24af8ab3de518c937db0df95
Léo Texier <55082513+LeoTexier@users.noreply.github.com>
2019-11-12 09:35:11 +0100

    addEv1

--- src/main/java/spring/s4/datasmap/MainController.java
+++ src/main/java/spring/s4/datasmap/MainController.java
@@ -1,33 +1,56 @@
-package spring.s4.datasmap;
+package spring.s4.datamap.controllers;
+
 
 
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+//import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import io.github.jeemv.springboot.vuejs.VueJS;
+//import io.github.jeemv.springboot.vuejs.VueJS;
 import spring.s4.datasmap.model.User;
 import spring.s4.datasmap.repositories.UserRepo;
+
+
+
 
 @Controller
 @RequestMapping("/test/")
 public class MainController {
-
 	
 	// Accès à la page d'acceuil
 	
 	@Autowired
+	private VueJS vue;
+	
+	@Autowired
 	private UserRepo userRepo;
 	
-	@GetMapping("/index")
-	public String index() {
-		User u = new User();
-		u.setName("nom");
-		u.setLastName("Dalton");
-		u.setEmail("joe.dalton@prison.org");
+	
+	@GetMapping("/inscription")
+	public String inscription() {
+		return ("inscription");
+	}
+	
+	@PostMapping("/validationinscription")
+	public String inscription(User user) {
+		userRepo.save(user);
+		return("validationinscription");		
+	}
+	
+	
+	@GetMapping("/connexion")
+		public String connexion(ModelMap model) {
+		vue.addData("valid", true);
+		vue.addData("name", "");
+		vue.addDataRaw("nameRules","[\n" + "v => !!v || 'Name is required',\n" +"v => (v && v.length <= 10) || 'Name must be less than 10 characters',\n" +"]");
 		
-		return ("index");
 		
+		return ("connexion");
 	}
 }
